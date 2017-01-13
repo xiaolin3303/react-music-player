ecabf95d5f078f829dbe573fbea4202630127663
xiaolin3303 <xiaolin3303@163.com>
2017-01-13 17:52:56 +0800

    dev env

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    devtool: 'eval-source-map',
+    entry: [
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
+        'react-hot-loader/patch',
+        path.join(__dirname, 'app/index.js')
+    ],
+    output: {
+        path: path.join(__dirname, '/dist/'),
+        filename: '[name].js',
+        publicPath: '/'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: './app/index.tpl.html',
+          inject: 'body',
+          filename: './index.html'
+        }),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new webpack.DefinePlugin({
+          'process.env.NODE_ENV': JSON.stringify('development')
+        })
+    ],
+    module: {
+        resolve:{
+            extensions:['','.js','.json']
+        },        
+        loaders: [
+            {
+              test: /\.js$/,
+              exclude: /node_modules/,
+              loader: "babel-loader",
+              query:
+                {
+                  presets:['react','es2015']
+                }
+            },
+            {
+                test: /\.json?$/,
+                loader: 'json'
+            },
+            {
+                test: /\.css$/,
+                loader: "style!css"
+            },
+            {
+                test: /\.less/,
+                loader: 'style-loader!css-loader!less-loader'
+            }
+        ]
+    }
+};
